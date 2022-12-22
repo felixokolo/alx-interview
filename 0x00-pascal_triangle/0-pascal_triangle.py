d868c2726e25dd10c9180f0d0c1a298a724416a3
felixokolo <pheelix.info@gmail.com>
2022-12-22 12:28:09 +0100

    First commit

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Pascal's triangle """
+
+def pascal_triangle(n):
+    """
+    Function that returns a list of integers
+    representing the Pascal's triangle of n
+        Parameters:
+            n (int): Integer representing size
+            Pascal's triangle
+
+        Returns:
+            triangle (list): List of integers
+    """
+
+    triangle = []
+    if (n <= 0):
+        return triangle
+    triangle = [[1]]
+    if (n == 1):
+        return triangle
+    for i in range(1, n):
+        next = []
+        pres = 0
+        for x in triangle[-1]:
+            next.append(pres + x)
+            pres = x
+        next.append(pres)
+        triangle.append(next)
+    return triangle
---
+++ README.md
@@ -0,0 +1,4 @@
+Alx Software Engineering interview questions
+
+Authors
+Okolo Chukwudi Felix https://github.com/felixokolo
