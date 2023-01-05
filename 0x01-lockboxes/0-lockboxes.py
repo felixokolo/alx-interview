b5dcafe31829f43218ff605d84d03d219e2e797e
felixokolo <pheelix.info@gmail.com>
2023-01-05 14:04:21 +0100

    correction

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,51 +1,64 @@
 #!/usr/bin/python3
 """ Lockboxes """
 
+
 def open_boxes(keys, boxes):
-  ret = []
-  for box in boxes:
-    if box['id'] in keys:
-      box['state'] = 'opened'
-      ret += box['keys']
-  return set(ret)
+    """
+    Open all boxes in in boxes using keys
 
-
-def canUnlockAll(boxes: list) -> bool:
-  """
-  Checks if all boxes in an array can be opened
-
-  Parameters:
+    Parameters:
+    keys (list): list of keys to open boxes
     boxes (list): list of list of keys
 
     Returns:
-      bool true if all boxes can be opened
-      fales otherwise
-  """
+        Set of new keys
+    """
+    ret = []
+    for box in boxes:
+        if box['id'] in keys:
+            box['state'] = 'opened'
+            ret += box['keys']
+    return set(ret)
 
-  if len(boxes) == 0:
-    return True
 
-  keys = []
+def canUnlockAll(boxes: list) -> bool:
+    """
+    Checks if all boxes in an array can be opened
 
-  if type(boxes[0]) is list:
-    boxes_dict = [{'id': x, 'keys': boxes[x], 'state': 'opened' if x == 0 else 'closed'}
-                  for x in range(len(boxes))]
-  else:
-    boxes_dict = boxes
-  opened = list(filter(lambda ele: ele['state'] == 'opened', boxes_dict))
-  closed = list(filter(lambda ele: ele['state'] == 'closed', boxes_dict))
-  
-  for ele in opened:
-    keys += ele['keys']
-  
-  if len(closed) > 0:
-    new_keys = open_boxes(keys, closed)
-    if len(new_keys) == 0:
-      new_closed = list(filter(lambda ele: ele['state'] == 'closed', closed))
-      if len(new_closed) > 0:
-        return False
-      else:
+    Parameters:
+        boxes (list): list of list of keys
+
+        Returns:
+            bool true if all boxes can be opened
+            fales otherwise
+    """
+
+    if len(boxes) == 0:
         return True
-  else:
-     return True
-  return canUnlockAll(closed)
+
+    keys = []
+
+    if type(boxes[0]) is list:
+        boxes_dict = [{'id': x, 'keys': boxes[x],
+                       'state': 'opened' if x == 0 else 'closed'}
+                      for x in range(len(boxes))]
+    else:
+        boxes_dict = boxes
+    opened = list(filter(lambda ele: ele['state'] == 'opened', boxes_dict))
+    closed = list(filter(lambda ele: ele['state'] == 'closed', boxes_dict))
+
+    for ele in opened:
+        keys += ele['keys']
+
+    if len(closed) > 0:
+        new_keys = open_boxes(keys, closed)
+        if len(new_keys) == 0:
+            new_closed = list(filter(lambda ele: ele['state'] == 'closed',
+                                     closed))
+            if len(new_closed) > 0:
+                return False
+            else:
+                return True
+    else:
+        return True
+    return canUnlockAll(closed)
