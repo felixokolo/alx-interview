7fe4a5686003e3dc27010a6b7283f435e704cbb6
felixokolo <pheelix.info@gmail.com>
2023-01-19 18:11:39 +0100

    correction

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,8 +3,14 @@
 Statistics calculation
 """
 import sys
+import re
 
 stdin = sys.stdin
+rex = (r'^([0-9]{1,3}\.){3}\d{1,3}\s-\s\[\d{4}' +
+       r'-[0-1][0-2]-[0-3]\d\s[0-2][0-9]:([0-5]' +
+       r'[0-9]:?){2}\.\d*\]\s"GET \/projects\/260 ' +
+       r'HTTP\/1\.1"\s\d{3}\s\d+$')
+rex2 = r'\d{3}\s\d+$'
 status_codes = {200: 0, 301: 0,
                 400: 0, 401: 0,
                 403: 0, 404: 0,
@@ -13,30 +19,28 @@ status_codes = {200: 0, 301: 0,
 total_size = 0
 loops = 0
 
+""" def handle(sign, 
+        print(line)frame):
+    global pressed
+    sys.stdout.flush()
+    to_print = 'File size: {}\n'.format(total_size)
+    sorted_keys = list(status_codes.keys())
+    sorted_keys.sort()
+
+    for k in sorted_keys:
+        to_print += f'{k}: {status_codes[k]}\n'
+        sys.stdout.write(to_print)
+
+signal.signal(signal.SIGINT, handle) """
+
 try:
     for line in stdin:
-        found = line.split(' ')
-        if len(found[0]) < 4:
-            continue
-        if found[1] != '-':
-            continue
-        if found[2][0] != '[' or found[3][-1] != ']':
-            continue
-        if found[4] != '"GET':
-            continue
-        if found[5] != '/projects/260':
-            continue
-        if found[6] != 'HTTP/1.1"':
-            continue
+        found = re.search(rex, line)
         if found is not None:
-            code = 0
-            size = 0
-            if found[-2].isnumeric():
-                code = int(found[-2])
-            if found[-1].strip().isnumeric():
-                size = int(found[-1])
-            status_codes[code] += 1
-            total_size += size
+            code_size = re.search(rex2, found.string)
+            code, size = found.string[code_size.start():].strip().split(' ')
+            status_codes[int(code)] += 1
+            total_size += int(size)
         loops += 1
         if loops == 10:
             print('File size: {}'.format(total_size))
@@ -44,13 +48,14 @@ try:
             sorted_keys.sort()
             for k in sorted_keys:
                 if status_codes[k] > 0:
-                    print('{}: {}'.format(k, status_codes[k]))
+                    print(f'{k}: {status_codes[k]}')
             loops = 0
 except(KeyboardInterrupt):
-    print('File size:', total_size)
+    to_print = 'File size: {}\n'.format(total_size)
     sorted_keys = list(status_codes.keys())
     sorted_keys.sort()
     for k in sorted_keys:
         if status_codes[k] > 0:
-            print('{}: {}'.format(k, status_codes[k]))
+            to_print += f'{k}: {status_codes[k]}\n'
+    print(to_print, end='')
     raise
---
+++ 0x03-log_parsing/0-stats2.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+Statistics calculation
+"""
+import sys
+
+stdin = sys.stdin
+status_codes = {200: 0, 301: 0,
+                400: 0, 401: 0,
+                403: 0, 404: 0,
+                405: 0, 500: 0
+                }
+total_size = 0
+loops = 0
+
+try:
+    for line in stdin:
+        found = line.split(' ')
+        if len(found[0]) < 4:
+            continue
+        if found[1] != '-':
+            continue
+        if found[2][0] != '[' or found[3][-1] != ']':
+            continue
+        if found[4] != '"GET':
+            continue
+        if found[5] != '/projects/260':
+            continue
+        if found[6] != 'HTTP/1.1"':
+            continue
+        if found is not None:
+            code = 0
+            size = 0
+            if found[-2].isnumeric():
+                code = int(found[-2])
+            if found[-1].strip().isnumeric():
+                size = int(found[-1])
+            status_codes[code] += 1
+            total_size += size
+        loops += 1
+        if loops == 10:
+            print('File size: {}'.format(total_size))
+            sorted_keys = list(status_codes.keys())
+            sorted_keys.sort()
+            for k in sorted_keys:
+                if status_codes[k] > 0:
+                    print('{}: {}'.format(k, status_codes[k]))
+            loops = 0
+except(KeyboardInterrupt):
+    print('File size:', total_size)
+    sorted_keys = list(status_codes.keys())
+    sorted_keys.sort()
+    for k in sorted_keys:
+        if status_codes[k] > 0:
+            print('{}: {}'.format(k, status_codes[k]))
+    raise
