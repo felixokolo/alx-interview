5cf3de65842e619c91611820b78d7ee3c4088a2d
felixokolo <pheelix.info@gmail.com>
2023-01-19 19:44:33 +0100

    correction

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,18 +4,19 @@ Statistics calculation
 """
 import sys
 
-def printStatus(dic, size):
+def printStatus(status_codes, total_size):
     """ Prints information """
-    print("File size: {:d}".format(size))
-    for i in sorted(dic.keys()):
-        if dic[i] != 0:
-            print("{}: {:d}".format(i, dic[i]))
+    print('File size: {:d}'.format(total_size))
+    sorted_keys = sorted(status_codes.keys())
+    for k in sorted_keys:
+        if status_codes[k] > 0:
+            print('{}: {:d}'.format(k, status_codes[k]))
 
 stdin = sys.stdin
-status_codes = {200: 0, 301: 0,
-                400: 0, 401: 0,
-                403: 0, 404: 0,
-                405: 0, 500: 0
+status_codes = {'200': 0, '301': 0,
+                '400': 0, '401': 0,
+                '403': 0, '404': 0,
+                '405': 0, '500': 0
                 }
 total_size = 0
 loops = 0
@@ -34,7 +35,7 @@ try:
                 size = int(found[-1])
                 total_size += size
             if found[-2].isnumeric():
-                code = int(found[-2])
+                code = found[-2]
             if code in status_codes:
                 status_codes[code] += 1
 except(KeyboardInterrupt, Exception):
