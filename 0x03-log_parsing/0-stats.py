abdc5791017eac61b8e42e63532fa18879244bb1
felixokolo <pheelix.info@gmail.com>
2023-01-19 14:58:48 +0100

    correction

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -19,20 +19,38 @@ status_codes = {200: 0, 301: 0,
 total_size = 0
 loops = 0
 
-for line in stdin:
-    found = re.search(rex, line)
-    if found is not None:
-        code_size = re.search(rex2, found.string)
-        code, size = found.string[code_size.start():].strip().split(' ')
-        status_codes[int(code)] += 1
-        total_size += int(size)
-    loops += 1
-    if loops == 10:
-        print('File size:', total_size)
-        sorted_keys = list(status_codes.keys())
-        sorted_keys.sort()
-        for k in sorted_keys:
-            print(f'{k}: {status_codes[k]}')
-        loops = 0
-    else:
-        pass
+""" def handle(sign, frame):
+    global pressed
+    sys.stdout.flush()
+    to_print = 'File size: {}\n'.format(total_size)
+    sorted_keys = list(status_codes.keys())
+    sorted_keys.sort()
+    
+    for k in sorted_keys:
+        to_print += f'{k}: {status_codes[k]}\n'
+        sys.stdout.write(to_print)
+
+signal.signal(signal.SIGINT, handle) """
+
+try:
+    for line in stdin:
+        found = re.search(rex, line)
+        if found is not None:
+            code_size = re.search(rex2, found.string)
+            code, size = found.string[code_size.start():].strip().split(' ')
+            status_codes[int(code)] += 1
+            total_size += int(size)
+        loops += 1
+        if loops == 10:
+            print('File size:', total_size)
+            sorted_keys = list(status_codes.keys())
+            sorted_keys.sort()
+            for k in sorted_keys:
+                print(f'{k}: {status_codes[k]}')
+            loops = 0
+except(KeyboardInterrupt):
+    print('File size:', total_size)
+    sorted_keys = list(status_codes.keys())
+    sorted_keys.sort()
+    for k in sorted_keys:
+        print(f'{k}: {status_codes[k]}')
