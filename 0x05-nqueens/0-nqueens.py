21467cfca0e84d7fdc8c557d56e184f628312197
felixokolo <pheelix.info@gmail.com>
2023-02-02 18:58:47 +0100

    correction

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -15,31 +15,31 @@ if N < 4:
     print('N must be at least 4')
     exit(1)
 
+
 def get_enemies(pos):
     enemies = []
     for i in range(N):
         enemies.append([i, pos[1]])
         enemies.append([pos[0], i])
-        if pos[0] > pos[1]:
-            start = [max(pos)-min(pos), 0]
-        else:
-            start = [0, max(pos)-min(pos)]
-        while min(start) >= 0 and max(start) < 5:
-            enemies.append(start)
-            start[0] += 1
-            start[1] += 1
-        start = pos
-        while min(start) >= 0 and max(start) < 5:
-            start[0] -= 1
-            start[1] += 1
+    if pos[0] > pos[1]:
+        start = [max(pos)-min(pos), 0]
+    else:
+        start = [0, max(pos)-min(pos)]
+    while min(start) >= 0 and max(start) < N+1:
+        enemies.append(start.copy())
+        start[0] += 1
+        start[1] += 1
+    start = pos.copy()
+    while min(start) >= 0 and max(start) < N+1:
+        start[0] -= 1
+        start[1] += 1
+    start[0] += 1
+    start[1] -= 1
+    while min(start) >= 0 and max(start) < N+1:
+        enemies.append(start.copy())
         start[0] += 1
         start[1] -= 1
-        while min(start) >= 0 and max(start) < 5:
-            enemies.append(start)
-            start[0] += 1
-            start[1] += 1
     return enemies
-
 
 
 def accept(Parent, child):
@@ -50,6 +50,7 @@ def accept(Parent, child):
     else:
         return True
 
+
 def find_solution(Parent, pos):
     """ find solution of challenge"""
     if len(Parent) == N:
@@ -58,11 +59,15 @@ def find_solution(Parent, pos):
         if accept(Parent, [pos+1, i]):
             Parent.append([pos+1, i])
             ret = find_solution(Parent, pos+1)
-            if ret is not None:
-                break
+            if ret is None:
+                del Parent[-1]
+                continue
+            else:
+                return ret
     else:
         return None
     return ret
+
 
 for i in range(N):
     print(find_solution([[0, i]], 0))
