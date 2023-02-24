d701e47b1f432e948169de5ebb9769929e8e74e3
felixokolo <pheelix.info@gmail.com>
2023-02-24 17:58:49 +0100

    First commit

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""Gets the minimum coins to make change"""
+
+
+def makeChange(coins, total):
+    """Function to get minimum change"""
+    if total is None:
+        return 0
+    if len(coins) == 0:
+        return -1
+    sorted_coins = coins.copy()
+    sorted_coins.sort(reverse=True)
+    pres = 0
+    num = 0
+    for i in sorted_coins:
+        if (pres + i) > total:
+            continue
+        rem = total - pres
+        mul = rem // i
+        if mul == 0:
+            return -1
+        num += mul
+        pres += mul * i
+        if pres == total:
+            return num
+    return -1
